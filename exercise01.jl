04a5ada7e7bc5e6cf7ae02cd6ecb59c1888a7580
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2020-06-21 11:10:31 +0200

    Update exercise01.jl

--- exercise01.jl
+++ exercise01.jl
@@ -1,14 +1,14 @@
 # Exercise 01: Strings, Vectors and Functions
 # Jinghua Xu, 21. June 2020
 
-#=
-A function `myreverse` that takes a vector as an argument and returns a vector of the same length, with the order of the elements reversed.
-=#
+
+# A function `myreverse` that takes a vector as an argument and returns a vector of the same length, with the order of the elements reversed.
+
 function myreverse(vec)
     # reversed_vec = Int64[] to restrict element type to Int64
     reversed_vec = []
     for idx in range(1, length=length(vec))
-        push!(reversed_vec, vec[idx])
+        pushfirst!(reversed_vec, vec[idx])
     end
     return reversed_vec
 end
@@ -20,9 +20,9 @@ println(myreverse(vec1))
 vec2 = ['a','b','c','d','e']
 println(myreverse(vec2))
 
-#=
-Write a function `uppercaseWords` that takes a string as input, splits it at all whitespaces and returns a vector of the words that start with an uppercase letter.
-=#
+
+# Write a function `uppercaseWords` that takes a string as input, splits it at all whitespaces and returns a vector of the words that start with an uppercase letter.
+
 function uppsercaseWords(str)
     return [word for word in split(str) if isuppercase(word[1])]
 end
@@ -47,9 +47,9 @@ str = "The name is Julia"
 println(uppercaseWords(str))
 =#
 
-#=
-a function `prettyPlus` that takes to numbers as arguments and returns a string representating the equation with the addition on the left hand side and the string on the right hand side
-=#
+
+# a function `prettyPlus` that takes to numbers as arguments and returns a string representating the equation with the addition on the left hand side and the string on the right hand side
+
 function prettyPlus(x,y)
     println("$x + $y = $(x+y)")
 end
